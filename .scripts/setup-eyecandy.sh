d7af1f3f7216f85fcac718a07d5e7f2b07aa2b5c
Hao Xiang Li <mark.haoxiang@gmail.com>
2023-09-26 10:42:29 +0100

    Eyecandy fix

--- .scripts/setup-eyecandy.sh
+++ .scripts/setup-eyecandy.sh
@@ -6,11 +6,19 @@
 gsettings set org.gnome.desktop.background picture-uri file://$HOME/.wallpapers/lake.jpg
 	# Dynamic wallpaper: 
 	# Default dark mode
-python ~/.wallpapers/dynamic_wallpaper.py lakeside
+python ~/.wallpapers/dynamic_wallpaper.py ~/.wallpapers/lakeside
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-CRONJOB="0 * * * * export DBUS_SESSION_BUS_ADDRESS=" + $DBUS_SESSION_BUS_ADDRESS + "; /usr/bin/python /home/markhaoxiang/.wallpapers/dynamic_wallpaper.py /home/markhaoxiang/.wallpapers/lakeside"
-crontab -l | { cat; echo $CRONJOB; } | crontab -
-sudo cp ~/.wallpapers/dynamic-wallpapers.desktop /usr/share/applications
+CRONJOB="export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS; /usr/bin/python $HOME/.wallpapers/dynamic_wallpaper.py $HOME/.wallpapers/lakeside"
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "0 * * * *" $CRONJOB >> mycron
+cat mycron
+#install new cron file
+crontab mycron
+rm mycron
+sudo cp ~/.wallpapers/dynamic-wallpaper.desktop /usr/share/applications
+
 
 # Themes
 yay -S gnome-themes-extra gnome-tweaks extension-manager papirus-icon-theme
