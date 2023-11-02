610b17e21e049e275ce9bca11a79e8ce350bda13
MarkHaoxiang <mark.haoxiang@gmail.com>
2023-11-02 12:22:22 +0000

    Merge

--- .scripts/setup-dev.sh
+++ .scripts/setup-dev.sh
@@ -4,10 +4,12 @@ git config --global user.email "mark.haoxiang@gmail.com"
 git config --global user.name "MarkHaoxiang"
 
 # Essentials
-yay -S vim visual-studio-code-bin surfshark-client p7zip bitwarden cronie github-cli
+yay -S vim visual-studio-code-bin surfshark-client p7zip bitwarden cronie github cli
+	# Latex
+yay -S texlive-latexrecommended texlive-latexextra
 sudo systemctl enable cronie.service --now
 # Docker
-yay -S docker
+yay -S docker docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
